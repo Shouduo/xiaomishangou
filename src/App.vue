6706b5b245398c3211bcdb70a1c476930eac79c8
unknown <handsomesaywhynot@gmail.com>
2020-02-16 19:46:03 +0800

    dropdown-cart style completed

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,50 @@
         <div class="topbar">
             <div class="container">
                 <div class="topbar-nav">
-                    <a href="">小米商城</a>
+                    <a class="nav-tag">小米商城</a>
                     <span>|</span>
-                    <a href="">MIUI</a>
+                    <a class="nav-tag">MIUI</a>
                     <span>|</span>
-                    <a href="">IoT</a>
+                    <a class="nav-tag">IoT</a>
                     <span>|</span>
-                    <a href="">云服务</a>
+                    <a class="nav-tag">云服务</a>
                     <span>|</span>
-                    <a href="">金融</a>
+                    <a class="nav-tag">金融</a>
                     <span>|</span>
-                    <a href="">有品</a>
+                    <a class="nav-tag">有品</a>
                     <span>|</span>
-                    <a href="">小爱开放平台</a>
+                    <a class="nav-tag">小爱开放平台</a>
                     <span>|</span>
-                    <a href="">政企服务</a>
+                    <a class="nav-tag">政企服务</a>
                     <span>|</span>
-                    <a href="">Select Region</a>
+                    <a class="nav-tag">Select Region</a>
                 </div>
-                <div class="topbar-cart"><a href=""><i class="iconfont">&#xe63b;</i>购物车（0）</a></div>
+                <div class="topbar-cart">
+                    <a class="nav-tag"><i class="iconfont">&#xe63b;</i>购物车（0）</a>
+                    <div class="dropdown-cart">
+                        <ul>
+                            <li>
+                                <img src="./assets/img/1.jpg" alt="">
+                                <a class="good-name"><em>【秒杀】</em>小米双肩包</a>
+                                <a class="delete" href="">&times;</a>
+                                <p class="good-price">19.9元 &times; 1</p>
+                            </li>
+                            <li>
+                                <img src="./assets/img/1.jpg" alt="">
+                                <a class="good-name"><em>【秒杀】</em>小米双肩包123123312</a>
+                                <a class="delete" href="">&times;</a>
+                                <p class="good-price">19.9元 &times; 1</p>
+                            </li>
+                        </ul>
+                        <div class="cart-summary clear-fixed">
+                            <div class="summary-left">
+                                <p class="summary-count">共 2 件商品</p>
+                                <p class="summary-money">238<em>元</em></p>
+                            </div>
+                            <a class="summary-pay">去购物车结算</a>
+                        </div>
+                    </div>
+                </div>
                 <div class="topbar-info clear-fixed">
                     <a href=""> 登录</a>
                     <span>|</span>
@@ -390,12 +415,15 @@ a {
 .topbar {
     background: #333333;
     height: 40px;
+    // position: relative;
+    // z-index: 1;
     a {
         color: #b0b0b0;
         font-size: 12px;
-    }
-    a:hover {
-        color: #fff;
+        cursor: pointer;
+        &:hover {
+            color: #fff;
+        }
     }
     .topbar-nav {
         float: left;
@@ -433,28 +461,147 @@ a {
     }
     .topbar-cart {
         float: right;
-        a {
+        position: relative;  
+        .nav-tag {
             display: block;
             height: 40px;
             line-height: 40px;
             text-align: center;
             width: 120px;
             background-color: #424242;
+            cursor: pointer;
+            transition-delay: .65s;
         }
-        a:hover {
+        &:hover .nav-tag {
+            transition-delay: 0s;
             background-color: #ffffff;
             color: #ff6700;
-        }
+            }
         i {
             line-height: 20px;
             font-size: 20px;
             padding-top: 2px;
+        }
+        &:hover .dropdown-cart {
+            transform:translate(0px, 0px);
+            max-height: 800px;
+            transition: max-height linear .6s, transform linear .3s;
+        }
+        .dropdown-cart {
+            display: block;
+            position: absolute;
+            width: 320px;
+            max-height: 0px;
+            right: 0;
+            overflow: hidden;
+            z-index: -1;
+            box-sizing: border-box;
+            background-color: #fff;
+            box-shadow: 0 10px 10px rgba($color: #000000, $alpha: 0.2); 
+            transform:translate(0px, -10px);
+            transition: max-height linear .6s, transform linear .3s .3s;
+            ul {
+                padding: 15px 15px 0;
+                max-height: 628px;
+                overflow: auto;
+                li {
+                    display: block;
+                    height: 80px;
+                    border-bottom: 1px solid #e0e0e0;
+                    &:hover .delete {
+                        opacity: 1;
+                    }
+                    img {
+                        float: left;
+                        width: 56px;
+                        margin: 12px 12px;
+                        vertical-align: middle;
+                    }
+                    .good-name {
+                        float: left;
+                        font-size: 12px;
+                        margin: 30px 0;
+                        color: #000;
+                        width: 120px;
+                        white-space: nowrap;
+                        overflow: hidden;
+                        text-overflow: ellipsis;
+                        cursor: pointer;
+                        em {
+                            color: #e05e40;
+                        }
+                        &:hover {
+                            color: #ff6700;
+                        }
+                    }
+                    .good-price {
+                        float: right;
+                        font-size: 12px;
+                        margin-right: 6px;
+                        margin-top: 20px;
+                    }
+                    .delete {
+                        opacity: 0;
+                        width: 16px;
+                        height: 16px;
+                        line-height: 80px;
+                        font-size: 20px;
+                        float: right;
+                        color: #b0b0b0;
+                        &:hover {
+                            color: #424242;
+                        }
+                    }
+                    &:last-child {
+                        border-bottom: none;
+                    }
+                }
+            }
+            .cart-summary {
+                background-color: #fafafa;
+                padding: 16px 20px 10px;
+                min-height: 72px;
+                box-sizing: border-box;
+                .summary-left {
+                    float: left;
+                    .summary-count {
+                        line-height: 12px;
+                        margin: 0;
+                        font-size: 12px;
+                        color: #757575;
+                    }
+                    .summary-money {
+                        margin: 0;
+                        color: #ff6700;
+                        line-height: 30px;
+                        font-size: 24px;
+                        font-weight: 400;
+                        em {
+                            font-style: normal;
+                            font-size: 12px;
+                        }
+                    }
+                }
+                .summary-pay {
+                    float: right;
+                    height: 40px;
+                    width: 140px;
+                    font-size: 14px;
+                    line-height: 40px;
+                    text-align: center;
+                    color: #fff;
+                    background-color: #ff6700;
+                    cursor: pointer;
+                }
+            }
         }
     }
 }
 
 .header {
     height: 100px;
+    position: relative;
+    z-index: -2;
     .header-logo {
         position: relative;
         float: left;
@@ -469,7 +616,6 @@ a {
             height: 55px;
             width: 110px; 
             transition: all ease-in-out .15s;
-            // background-color: #ff6700;
             &::before {
                 content: "";
                 position: absolute;
@@ -581,6 +727,8 @@ a {
 
 .seckill {
     background-color: #f5f5f5;
+    position: relative;
+    z-index: -2;
     .seckill-head {
         background: url("./assets/seckill-head.jpg") no-repeat 50% 0;
         height: 170px;
